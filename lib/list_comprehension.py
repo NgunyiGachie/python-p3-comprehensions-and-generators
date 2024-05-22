448bf8317d33caca93d19e58bfb58b36c6b81cfd
NgunyiGachie <antogachie@gmail.com>
2024-05-22 15:14:09 +0300

    Complete assignment

--- lib/list_comprehension.py
+++ lib/list_comprehension.py
@@ -1,7 +1,11 @@
 #!/usr/bin/env python3
 
 def return_evens(num_list):
-    pass
+    even_numbers = [x for x in num_list if x % 2 == 0]
+    print(even_numbers)
+    return even_numbers
 
 def make_exclamation(sentence_list):
-    pass
+    new_list = [x + "!" for x in sentence_list]
+    print(new_list)
+    return new_list
